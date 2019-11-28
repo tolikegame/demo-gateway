48613a36de6d84f1713120d4c6c4062529c2c2ce
Larry <tgfc045@tgfc.tw>
2019-11-28 16:02:47 +0800

    111

---
+++ client-service/src/test/java/com/example/clientservice/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ core-service/src/main/java/com/example/coreservice/intercepter/UserContextHolder.java
@@ -0,0 +1,16 @@
+package com.example.coreservice.intercepter;
+
+import com.example.coreservice.vo.User;
+
+public class UserContextHolder {
+	public static ThreadLocal<User> context = new ThreadLocal<User>();
+	public static User currentUser() {
+		return context.get();
+	}
+	public static void set(User user) {
+		context.set(user);
+	}
+	public static void shutdown() {
+		context.remove();
+	}
+}
